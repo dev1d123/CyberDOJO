ab1ed5b2b9c8c6c625669ac18d97e3c99682e9a8
wcarlosc <wcarlosc@unsa.edu.pe>
2026-01-10 19:08:32 -0500

    tabla de preferencias

--- backend/cyberkids/apps/cyberUser/admin.py
+++ backend/cyberkids/apps/cyberUser/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import CyberUser, Country, RiskLevel
+from .models import CyberUser, Country, RiskLevel, Preferences
 admin.site.register(CyberUser)
 admin.site.register(Country)
 admin.site.register(RiskLevel)
+admin.site.register(Preferences)
