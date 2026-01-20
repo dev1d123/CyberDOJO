5b092530a803e22a42504520a724d7d25bddf29a
Julio Chura <jchuraaca@unsa.edu.pe>
2026-01-20 09:02:20 -0500

    cors

--- backend/cyberkids/cyberkids/settings.py
+++ backend/cyberkids/cyberkids/settings.py
@@ -170,7 +170,28 @@ SIMPLE_JWT = {
     "USER_ID_CLAIM": "user_id",
 }
 
+# CORS Configuration
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 # Hosts permitidos
 ALLOWED_HOSTS = ['*']
