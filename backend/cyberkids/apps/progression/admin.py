baab0b3bd4346ebb6bb3b4f5ba1fa42515e18e42
wcarlosc <wcarlosc@unsa.edu.pe>
2026-02-27 04:44:05 -0500

    admin for Archievements

--- backend/cyberkids/apps/progression/admin.py
+++ backend/cyberkids/apps/progression/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import ProgressionLevel, CosmeticItem, UserInventory, CreditTransaction, UserProgress
+from .models import ProgressionLevel, CosmeticItem, UserInventory, CreditTransaction, UserProgress, Achievement, UserAchievement
+
 
 admin.site.register(ProgressionLevel)
 admin.site.register(CosmeticItem)
 admin.site.register(UserInventory)
 admin.site.register(CreditTransaction)
 admin.site.register(UserProgress)
+admin.site.register(Achievement)
+admin.site.register(UserAchievement)
